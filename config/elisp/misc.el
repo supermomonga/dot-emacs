50caba66974bde01cc32a6112259a16d2133c92e
supermomonga <hi@supermomonga.com>
2016-04-30 11:24:52 +0900

    src dir for each pc

--- config/elisp/misc.el
+++ config/elisp/misc.el
@@ -1,2 +1,6 @@
 
-(setq find-function-C-source-directory "/Users/momonga/Develops/sources/emacs-24.5/src")
+
+(setq find-function-C-source-directory
+      (case (user-original-login-name)
+        "momonga" "/Users/momonga/Develops/sources/emacs-24.5/src"
+        "fax" "~/dev/emacs/src"))
